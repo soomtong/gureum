3c533f5c42bac617680dbcc8a6ffe13c9c1e090a
soomtong <soomtong@gmail.com>
2016-04-26 00:22:44 +0900

    removed crashlytics

--- OSX/GureumAppDelegate.m
+++ OSX/GureumAppDelegate.m
@@ -6,7 +6,6 @@
 //  Copyright 2011 youknowone.org. All rights reserved.
 //
 
-//#import <Crashlytics/Crashlytics.h>
 #import "GureumAppDelegate.h"
 
 #import "CIMInputManager.h"
@@ -18,8 +17,6 @@
 @synthesize menu;
 
 - (void)awakeFromNib {
-    //[Crashlytics startWithAPIKey:@"1b5d8443c3eabba778b0d97bff234647af846181"];
-
     self->sharedInputManager = [[CIMInputManager alloc] init];
 
     NSDictionary *versionInfo = [[GureumAppDelegate sharedAppDelegate] getRecentVersion];
